1597499ff2f72502b55d7541599b80a58607b8ad
Isabella Steen <Isabella Steen@LAPTOP-Q8CR7GRV>
2018-02-16 13:22:18 +0100

    asd

--- Sudoku/src/sudoku/Interface.java
+++ Sudoku/src/sudoku/Interface.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-// Isabellas komentar
+// Isabellas kommentar hej
 
 public class Interface extends Application {
 
